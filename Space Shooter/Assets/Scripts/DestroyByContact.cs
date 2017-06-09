a3dcbe27dd7c1376825d634cafa4ddca2dd62b02
Edgar Muniz Berlinck <edgar.berlinck@inflor.com.br>
2017-06-09 16:43:34 -0300

    Space Shooter Enemies

--- Space Shooter/Assets/Scripts/DestroyByContact.cs
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject palyerExplosion;
+
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Boundary")
 			return;
+		Instantiate (explosion, transform.position, transform.rotation);
+		if (other.tag == "Player") { 
+			Instantiate (palyerExplosion, other.transform.position, other.transform.rotation);
+		}
 		Destroy (other.gameObject);
 		Destroy (gameObject);
 	}
--- Space Shooter/Assets/Scripts/PlayerController.cs
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -21,7 +21,7 @@ public class PlayerController : MonoBehaviour {
 	{
 		if (Input.GetButton("Fire1") && Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
-			GameObject clone = Instantiate (shot, shotSpawn.position, shotSpawn.rotation) as GameObject;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
 		}
 	}
 
