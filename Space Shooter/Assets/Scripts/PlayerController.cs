d78b97f148256988072fda93e8fad582fd149d13
Edgar Muniz Berlinck <edgar.berlinck@inflor.com.br>
2017-06-30 16:57:37 -0300

    Mobile version

--- Space Shooter/Assets/Scripts/PlayerController.cs
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -17,6 +17,12 @@ public class PlayerController : MonoBehaviour {
 	public float fireRate = 0.5f;
 	private float nextFire = 0.0f;
 
+	private Quaternion calibrationQuaternion;
+
+	void Start() {
+		CalibrateAccelerometer ();
+	}
+
 	void Update() 
 	{
 		if (Input.GetButton("Fire1") && Time.time > nextFire) {
@@ -29,10 +35,13 @@ public class PlayerController : MonoBehaviour {
 
 	void FixedUpdate() 
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		//float moveHorizontal = Input.GetAxis ("Horizontal");
+		//float moveVertical = Input.GetAxis ("Vertical");
+		//Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		Vector3 accelerationRaw = Input.acceleration;
+		Vector3 acceleration = FixAcceleration (accelerationRaw);
+		Vector3 movement = new Vector3 (acceleration.x, 0.0f, acceleration.y);
 
 		Rigidbody body = gameObject.GetComponent (typeof(Rigidbody)) as Rigidbody;
 		body.velocity = movement * speed;
@@ -42,6 +51,19 @@ public class PlayerController : MonoBehaviour {
 			Mathf.Clamp (body.position.z, boundary.zMin, boundary.zMax));
 
 		body.rotation = Quaternion.Euler (0.0f, 0.0f, body.velocity.x * -tilt);
+	}
+
+	void CalibrateAccelerometer() 
+	{
+		Vector3 accelerationSnapshot = Input.acceleration;
+		Quaternion rotationQueternion = Quaternion.FromToRotation (new Vector3 (0.0f, 0.0f, -1.0f), accelerationSnapshot);
+		calibrationQuaternion = Quaternion.Inverse (rotationQueternion);
+	}
+
+	Vector3 FixAcceleration (Vector3 acceleration) 
+	{
+		Vector3 fixedAcceleration = calibrationQuaternion * acceleration;
+		return fixedAcceleration;
 	}
 		
 }
